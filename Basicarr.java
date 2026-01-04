9215c4023b8a183c06940b38b402fb25636979ae
abhishektiwari2918 <abhishektiwari5467@gmail.com>
2026-01-04 20:07:38 +0530

    first commit

---
+++ Basicarr.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class Basicarr {
+    public static void main(String[] args) {
+        int arr[]={20,23,26,29};
+        int arr2[]={20,23,26,29};
+        // boolean isequql=true;
+        // if(arr.length!=arr2.length){
+        //     isequql=false;
+        // }else{
+        //     for(int i=0;i<arr.length;i++){
+        //       if(  arr[i]!=arr2[i]){
+
+              
+        //         isequql=false;
+        //         break;
+        //        }
+        //     }
+        // }
+
+        System.out.println(Arrays.equals(arr,arr2));
+    
+    
+    }
+}
---
+++ Myarrats.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class Myarrats {
+    public static void main(String[] args) {
+        int fees[]={0,4000,2000,3000,1200};
+        System.out.println(fees.length);
+        System.out.println(Arrays.toString(fees).length());
+         // print all elements by a loop
+    // print square of each element 
+
+// 
+
+
+
+
+
+
+
+
+    }
+    
+}
